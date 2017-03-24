cd85a58debdca2102d15b3d294176a2f21ca9935
Matthew Basset <matthew@quandl.com>
2017-03-24 10:19:03 -0400

    Fix up some basic tests.

--- spec/quandl/quandl-config_spec.rb
+++ spec/quandl/quandl-config_spec.rb
@@ -1,19 +1,27 @@
 require 'spec_helper'
 
 describe Quandl::Config do
+  subject(:configuration) { Quandl::Config.new('fake') }
+
   describe '.initialize' do
     it 'generates a Config class, evaluating ERB' do
-      configuration = Quandl::Config.new('fake')
-
       expect(configuration['notifiers']['ci']['channel']).to eq('#development')
+    end
+  end
+
+  describe 'accessing values' do
+    it 'can access values as methods' do
+      expect(configuration['notifiers']['ci']['channel']).to eq('#development')
+    end
+
+    it 'can access values as hash table' do
+      expect(configuration.notifiers['ci']['channel']).to eq('#development')
     end
   end
 
   describe '#configurable_attributes' do
     it 'provides a list of configurable attributes' do
-      configuration = Quandl::Config.new('fake')
-
-      expect(configuration.configurable_attributes).to eq([:webhook_url, :notifiers])
+      expect(configuration.configurable_attributes).to match_array([:webhook_url, :notifiers])
     end
   end
 end
